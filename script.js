c801e4aaede0098ff364181a215d6dda0046d910
ckauj <59056875+ckauj@users.noreply.github.com>
2025-02-12 11:21:55 -0500

    Add close event to dialog, when confirm clicked call addBookToLibrary() ... when cancel clicked do nothing

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const myBookshelf = [];
 const addNewBookDialog = document.getElementById('add-book-dialog');
 const addBookBtn = document.getElementById('add-book-btn');
+const confirmBtn = document.querySelector('#add-book-confirm');
+const bookTitleInput = document.querySelector('#book-title');
+const bookAuthorInput = document.querySelector('#book-author');
+const bookPageCountInput = document.querySelector('#book-page-count');
+const bookStatusInput = document.querySelector('#book-status');
 
+
+// When add book button is clicked, shows dialog form
 addBookBtn.addEventListener("click", () => {
     addNewBookDialog.showModal();
 });
 
+// This is triggered when 'cancel' is clicked, or from the ...
+// ... close event when clicking 'confirm'
+// If 'cancel' clicked it will not execute the 'if'
+// Values reset to original after
+addNewBookDialog.addEventListener("close", () => {
+    if(bookAuthorInput.value !== "" && bookTitleInput.value !== "") {
+        addBookToLibrary(bookTitleInput.value, bookAuthorInput.value, 
+            bookPageCountInput.value, bookStatusInput.value);
+    }
+    bookTitleInput.value = "";
+    bookAuthorInput.value = "";
+    bookPageCountInput.value = "";
+    bookStatusInput.value = "reading";
+});
+
+// Prevents the confirm button from submitting the form
+// Triggers the 'close' event when cofirm is clicked
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    addNewBookDialog.close();
+});
 
 function Book(title, author, pageCount, status) {
     this.title = title;
@@ -14,6 +42,9 @@ function Book(title, author, pageCount, status) {
     this.status = status;
 }
 
+// Creates new instance of Book and adds it to array
+// Creates new card w/ title, author, page # and status
+// Adds new card to container
 function addBookToLibrary(title, author, pageCount, status) {
     let newBook = new Book(title, author, pageCount, status);
 
